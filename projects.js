3c8b0eaafb40c95a3480fc71037ede9273b22c25
piyush <133100406+piyxshh@users.noreply.github.com>
2024-08-10 11:01:58 +0000

    begn

---
+++ projects.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+    
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("invalid deposit, try again.");
+        } else {
+            return numberDepositAmount;
+        }
+    } 
+ 
+}
+
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("enter number of lines to bet on(1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    } 
+}
+
+const depositAmount = deposit(); 
+//console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
+//console.log(numberOfLines);
